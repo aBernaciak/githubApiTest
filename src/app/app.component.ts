4b23a5b79fa8b16ea8ae6566e06869f976db5f30
Sintari <aleks.bernaciak@gmail.com>
2017-09-26 01:30:09 +0200

    Added comments and file for app to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,24 @@ import { MdSnackBar } from '@angular/material';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  message = '';
-  showList = false;
-  loading = false;
-  user = {
+  message = ''; //error message
+  showList = false; // when to show list with repositories
+  loading = false; // loading state
+  user = {  //user object
     githubPage: '',
     imgUrl: '',
     imgAlt: ''
   };
-  data = [];
+  data = [];  //data loaded from github API
 
   constructor(private http: Http, public snackBar: MdSnackBar) {}
 
-  callApi(githubUser) {
+  callApi(githubUser) { //function for calling API
     if (githubUser != '') {
       this.loading = true;
       this.http.request(`https://api.github.com/users/${githubUser}/repos`)
       .subscribe(
-        result => {
+        result => { // success
           if (result.json().length !== 0) {
             this.data = result.json();
             this.user.imgUrl = result.json()[0].owner.avatar_url;
@@ -36,12 +36,12 @@ export class AppComponent {
             this.snackBar.open('Data sucessfully downloaded.', 'Close');
             console.log(this.data)
           }
-          else {
+          else { // error handling
             this.clearData();
             this.snackBar.open(`User '${githubUser}' doesnt have any repositories.`, 'Close');
           }
         },
-        error => {
+        error => {  // error handling
           this.clearData();
           this.message = error.statusText;
           this.message = (this.message == '' ? 'Unknown Error' : error.statusText);
@@ -49,11 +49,11 @@ export class AppComponent {
         },
       );
     }
-    else {
+    else {  // error handling
       this.snackBar.open('You have to fill input field.', 'Close');
     }
   }
-  clearData() {
+  clearData() {  //clearing data
     this.showList = false;
     this.data = [];
     this.user.imgUrl = '';
